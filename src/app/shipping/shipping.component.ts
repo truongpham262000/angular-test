107ab37cef8e357d9e82d3d6a5c66a0e41fee09b
truongpham262000 <71928711+truongpham262000@users.noreply.github.com>
2023-02-10 15:46:50 +0700

    1 file modified

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from '../products';
 
 @Component({
   selector: 'app-shipping',
   templateUrl: './shipping.component.html',
-  styleUrls: ['./shipping.component.css']
+  styleUrls: ['./shipping.component.css'],
 })
-export class ShippingComponent {
-
-}
+export class ShippingComponent {}
